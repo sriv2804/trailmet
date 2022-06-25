622e9fd3ccce14f19147513ae7d668eada70ca79
Rishabh Tiwari <akchitra99@gmail.com>
2022-06-25 07:25:55 +0000

    updated resnet

--- trailmet/models/__init__.py
+++ trailmet/models/__init__.py
@@ -3,7 +3,7 @@ from .resnet import get_resnet_model
 
 class ModelsFactory(object):
     @staticmethod
-    def create_model(**kwargs):
+    def create_model(name, num_classes=100, pretrained = False, **kwargs):
         """
         Returns the requested model, ready for training/pruning with the specified method
 
@@ -13,15 +13,11 @@ class ModelsFactory(object):
         Return:
             model object
         """
-        assert 'name' in kwargs, "should provide model name"
-        assert 'num_classes' in kwargs, "should provide number of classes"
-        name = kwargs['name']
-        num_classes = kwargs['num_classes']
 
         if 'resnet' in name:
             assert 'insize' in kwargs, "should provide input size"
             insize = kwargs['insize']
-            model = get_resnet_model(name, num_classes, insize)
+            model = get_resnet_model(name, num_classes, insize, pretrained = pretrained, **kwargs)
         else:
             raise Exception("unknown model {}".format(kwargs['name']))
         return model
